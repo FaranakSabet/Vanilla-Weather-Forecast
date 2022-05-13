60a9c964b76cb34868a61c5811c6b9b8e9d79b3f
Faranak Sabet <65681350+FaranakSabet@users.noreply.github.com>
2022-05-12 17:38:56 -0700

    added daily forcast

--- src/app.js
+++ src/app.js
@@ -21,10 +21,11 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+    console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
   let days = ["Tue", "Wed", "Thu"];
-  
+
   let forecastHTML = `<div class = "row">`;
 
   days.forEach(function (day) {
@@ -44,6 +45,14 @@ function displayForecast() {
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+}
+
+
+function getForecast(coordinates){
+console.log(coordinates);
+let apiKey = "80d01cc78c74d5f3d38068b3ed2c0ba7";
+let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units = metric`;
+axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -68,6 +77,8 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -112,4 +123,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
-displayForecast();
+
